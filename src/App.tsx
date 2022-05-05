7f9b0a16a25ff90e18c099714e2e9f21ffaf74c9
pedrokpdo <pedromotoehm@gmail.com>
2022-05-05 13:53:02 -0300

    styled-components && types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import * as C from './App.styles'
+const App = (
 
-const App = () => {
+) => {
+
   return (
-    
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+
+      {/* Area de info */}
+
+
+      {/* Area de incerçao */}
+
+
+      {/* Tabela de itens */}
+
+
+      </C.Body>
+    </C.Container>
   )
 }
+
+export default App
