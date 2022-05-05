4f0e332c0e9d2363d1fa1e74331fc5d9fa4a50ec
pedrokpdo <pedromotoehm@gmail.com>
2022-05-05 17:12:36 -0300

    terminando styles

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,33 @@ const App = () => {
   const [list, setList] = useState(items);
   const [filtredList, setFiltredList] = useState<Item[]>([])
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
+
   console.log(list);
   
   
   useEffect(() => {
     setFiltredList(filterListByMonth(list, currentMonth))
   },[list, currentMonth])
+
+  useEffect(() => {
+    let incomeCont = 0 
+    let expenseCont = 0
+
+    for(let i in filtredList) {
+      if (categories[filtredList[i].category].expense) {
+        expenseCont += filtredList[i].value
+      } else {
+        incomeCont += filtredList[i].value
+
+      }
+    }
+
+    setIncome(incomeCont)
+    setExpense(expenseCont)
+
+  },[filtredList])
 
   const handleMonthChange = (newMonth:string) => {
     setCurrentMonth(newMonth)
@@ -35,6 +56,8 @@ const App = () => {
       <InfoArea 
        currentMonth={currentMonth}
        onMonthChange={handleMonthChange}
+       income={income}
+       expense={expense}
        />
 
 
--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,25 +1,29 @@
 import * as C from './style'
-import {formatCurrentMonth} from '../../helpers/dateFilter'
+import { formatCurrentMonth } from '../../helpers/dateFilter'
+import { ResumeItem } from '../ResumeItem';
 
 type Props = {
-    currentMonth:string;
+    currentMonth: string;
     onMonthChange: (newMonth: string) => void
+    income: number;
+    expense: number;
+
 }
 
-export const InfoArea = ({currentMonth, onMonthChange}:Props) => {
+export const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props) => {
 
     const handlePrevMonth = () => {
         let [year, month] = currentMonth.split('-')
-        let currentDate = new Date(parseInt(year), parseInt(month) -1,1)
-        currentDate.setMonth( currentDate.getMonth() -1)
-        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() +1}`)
-        
-    } 
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1)
+        currentDate.setMonth(currentDate.getMonth() - 1)
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
+
+    }
     const handleNextMonth = () => {
         let [year, month] = currentMonth.split('-')
-        let currentDate = new Date(parseInt(year), parseInt(month) -1,1)
-        currentDate.setMonth( currentDate.getMonth() +1)
-        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() +1}`)
+        let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1)
+        currentDate.setMonth(currentDate.getMonth() + 1)
+        onMonthChange(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`)
     }
 
 
@@ -31,7 +35,12 @@ export const InfoArea = ({currentMonth, onMonthChange}:Props) => {
                 <C.MonthArrow onClick={handleNextMonth}>➡️</C.MonthArrow>
             </C.MonthArea>
             <C.ResumeArea>
-
+                <ResumeItem title='Receitas' value={income} />
+                <ResumeItem title='Despesas' value={expense} />
+                <ResumeItem
+                    title='Balanço'
+                    value={income - expense}
+                    color={(income - expense) < 0 ? 'red' : 'green'}/>
             </C.ResumeArea>
         </C.Container>
     )
